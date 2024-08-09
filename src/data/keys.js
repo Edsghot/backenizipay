9cdbdd6e3ef495432d84af6463a0a4a65e00f730
edsghot <edsghot@gmail.com>
2024-08-09 13:11:23 -0500

    main

---
+++ src/data/keys.js
@@ -0,0 +1,10 @@
+
+const keys = {
+    "endpoint": "https://api.micuentaweb.pe",
+    "username" : "85649863",
+    "password": "testpassword_lNGlGed5aRXBh6PUAbyFpNCqS8BfDOL56v19WaNghxlAg",
+    "publickey": "85649863:testpublickey_AgmwCprcgDdTLqrOgQuHZIkHQ9lZuq1oM14KINQa1lPqH",
+    "HMACSHA256" : "qGY9A1JsvnjH2AOfiBgOoktWwm85sHI1KbAq7lzIRDx82",
+    
+}
+module.exports =  keys
